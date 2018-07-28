8f759ddda3fd4d9557fb84d35149e38f1de9a8f1
youichi <yofujimo@gmail.com>
2018-07-28 13:42:47 +0900

    minor: improve sample

--- examples/src/Suggest.tsx
+++ examples/src/Suggest.tsx
@@ -1,7 +1,42 @@
 import * as React from 'react';
 import * as fetchJsonp from 'fetch-jsonp';
+import styled from 'styled-components';
 
 import { StateAndUpdater, StateProvider, DebouncePropagator, AsyncResolver } from 'reenhance-components';
+
+
+const Dropdown = styled.div`
+  position: relative;
+  font: normal .8em sans-serif;
+`;
+
+const Input = styled.input`
+  margin: 0;
+  border: 1px solid silver;
+  border-radius: 3px;
+  font-family: inherit;
+  font-size: 100%;
+  -webkit-appearance: searchfield;
+`;
+
+const SuggestsUl = styled.ul`
+  position: absolute;
+  z-index: 1;
+  top: 5px;
+  padding: 0;
+  border: 1px solid silver;
+  background-color: white;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  list-style: none;
+`;
+
+const SuggestsLi = styled.li`
+  padding: 0 10px;
+  cursor: pointer;
+  &:hover {
+    background-color:silver;
+  }
+`;
 
 
 const queryToUrl =
@@ -15,14 +50,14 @@ const asyncFetch =
 
 const SuggestAsyncResolver = AsyncResolver<any, { query: string }>('query', []);
 
-const Suggests = ({ query }: any) => (
+const Suggests = ({ query, setQuery }: any) => (
   <SuggestAsyncResolver query={query} subject={asyncFetch}>
     {props => (
-      <ul>
+      <SuggestsUl>
         {props[1] && props[1].length > 0 ? props[1].map((str: string) => (
-          <li key={str}>{str}</li>
+          <SuggestsLi key={str} onClick={() => setQuery(str)}>{str}</SuggestsLi>
         )) : <li>No results</li>}
-      </ul>
+      </SuggestsUl>
     )}
   </SuggestAsyncResolver>
 );
@@ -33,19 +68,15 @@ const SuggestDebounce = DebouncePropagator<{ query: string }>({ query: '' });
 export const SuggestedInput = () => (
   <InputState>
     {({ state: query, setState: setQuery }: StateAndUpdater<string>) => (
-      <div>
-        <input value={query} onChange={e => setQuery(e.target.value)} />
+      <Dropdown>
+        <Input type="search" value={query} onChange={e => setQuery(e.target.value)} />
         <SuggestDebounce
           time={200}
           query={query}
         >
-          {({ query, state }: any) => (
-            <div>
-              {query && <Suggests query={query}/>}
-            </div>
-          )}
+          {({ query }: any) => query && (<Suggests query={query} setQuery={setQuery} />)}
         </SuggestDebounce>
-      </div>
+      </Dropdown>
     )}
   </InputState>
 );
